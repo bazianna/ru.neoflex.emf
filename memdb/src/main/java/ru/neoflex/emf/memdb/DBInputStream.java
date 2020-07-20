2f4e539970422c78b604c4a8f39076864ae2064a
arch7tect <arch7tect@gmail.com>
2020-07-20 11:17:04 +0300

    initial commit

---
+++ memdb/src/main/java/ru/neoflex/emf/memdb/DBInputStream.java
@@ -0,0 +1,31 @@
+package ru.neoflex.emf.memdb;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+public class DBInputStream extends InputStream implements URIConverter.Loadable {
+    private DBTransaction tx;
+    private URI uri;
+    private Map<?, ?> options;
+
+    public DBInputStream(DBTransaction tx, URI uri, Map<?, ?> options) {
+        this.tx = tx;
+        this.uri = uri;
+        this.options = options;
+    }
+
+    @Override
+    public int read() throws IOException {
+        return 0;
+    }
+
+    @Override
+    public void loadResource(Resource resource) throws IOException {
+        tx.load(resource);
+    }
+}
---
+++ memdb/src/main/java/ru/neoflex/emf/memdb/DBOutputStream.java
@@ -0,0 +1,30 @@
+package ru.neoflex.emf.memdb;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.URIConverter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+public class DBOutputStream extends OutputStream implements URIConverter.Saveable {
+    private DBTransaction tx;
+    private URI uri;
+    private Map<?, ?> options;
+
+    public DBOutputStream(DBTransaction tx, URI uri, Map<?, ?> options) {
+        this.tx = tx;
+        this.uri = uri;
+        this.options = options;
+    }
+
+    @Override
+    public void saveResource(Resource resource) throws IOException {
+        tx.save(resource);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+    }
+}
