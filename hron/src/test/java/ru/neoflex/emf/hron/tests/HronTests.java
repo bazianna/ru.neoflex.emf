f58191ae239ba37cef3963137ebede467f511324
asuvorova <mezzo666@mail.ru>
2021-12-29 15:37:42 +0300

    сщььше ркщт еуыеы

--- hron/src/test/java/ru/neoflex/emf/hron/tests/HronTests.java
+++ hron/src/test/java/ru/neoflex/emf/hron/tests/HronTests.java
@@ -19,63 +19,63 @@ import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public class HronTests {
-    public static final Logger logger = Logger.getLogger(HronTests.class.getName());
-    private final EPackage.Registry packageRegistry = new EPackageRegistryImpl(EPackage.Registry.INSTANCE);
-
-    {
-        packageRegistry.put(HronPackage.eINSTANCE.getNsURI(), HronPackage.eINSTANCE);
-    }
-
-    @Test
-    public void simpleTest() throws IOException, URISyntaxException {
-        HronResourceSet rs = new HronResourceSet();
-        rs.setPackageRegistry(packageRegistry);
-        URI uri1 = URI.createURI(this.getClass().getClassLoader().getResource("module1.hron").toString());
-        Resource resource1 = rs.createResource(uri1);
-        resource1.load(null);
-        URI uri2 = URI.createURI(this.getClass().getClassLoader().getResource("module2.hron").toString());
-        Resource resource2 = rs.createResource(uri2);
-        resource2.load(null);
-        rs.resolveAllReferences();
-        String s = resource1.toString();
-        logger.info(s);
-        String old = new String(
-                Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource("module1.hron").toURI())
-                ), StandardCharsets.UTF_8);
-        Assert.assertEquals(old, s);
-    }
-
-    @Test
-    public void ecoreTest() throws IOException, URISyntaxException {
-        HronResourceSet rs = new HronResourceSet();
-        URI uri = URI.createURI("ecore.hron");
-        Resource resource = rs.createResource(uri);
-        resource.getContents().add(EcoreUtil.copy(EcorePackage.eINSTANCE));
-        String s = resource.toString();
-        logger.info(s);
-        String old = new String(
-                Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource("ecore.hron").toURI())
-                ), StandardCharsets.UTF_8);
-        Assert.assertEquals(old, s);
-    }
-
-    @Test
-    public void eStringToStringMapEntryTest() throws IOException {
-        HronResourceSet rs = new HronResourceSet();
-        URI uri = URI.createURI("EStringToStringMapEntry.hron");
-        Resource resource = rs.createResource(uri);
-        resource.getContents().add(EcoreUtil.copy(EcorePackage.eINSTANCE.getEBigDecimal().getEAnnotations().get(0)));
-        String s = resource.toString();
-        logger.info(s);
-    }
-
-    @Test
-    public void hronTest() throws IOException {
-        HronResourceSet rs = new HronResourceSet();
-        URI uri = URI.createURI("HronPackage.hron");
-        Resource resource = rs.createResource(uri);
-        resource.getContents().add(EcoreUtil.copy(HronPackage.eINSTANCE));
-        String s = resource.toString();
-        logger.info(s);
-    }
+//    public static final Logger logger = Logger.getLogger(HronTests.class.getName());
+//    private final EPackage.Registry packageRegistry = new EPackageRegistryImpl(EPackage.Registry.INSTANCE);
+//
+//    {
+//        packageRegistry.put(HronPackage.eINSTANCE.getNsURI(), HronPackage.eINSTANCE);
+//    }
+//
+//    @Test
+//    public void simpleTest() throws IOException, URISyntaxException {
+//        HronResourceSet rs = new HronResourceSet();
+//        rs.setPackageRegistry(packageRegistry);
+//        URI uri1 = URI.createURI(this.getClass().getClassLoader().getResource("module1.hron").toString());
+//        Resource resource1 = rs.createResource(uri1);
+//        resource1.load(null);
+//        URI uri2 = URI.createURI(this.getClass().getClassLoader().getResource("module2.hron").toString());
+//        Resource resource2 = rs.createResource(uri2);
+//        resource2.load(null);
+//        rs.resolveAllReferences();
+//        String s = resource1.toString();
+//        logger.info(s);
+//        String old = new String(
+//                Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource("module1.hron").toURI())
+//                ), StandardCharsets.UTF_8);
+//        Assert.assertEquals(old, s);
+//    }
+//
+//    @Test
+//    public void ecoreTest() throws IOException, URISyntaxException {
+//        HronResourceSet rs = new HronResourceSet();
+//        URI uri = URI.createURI("ecore.hron");
+//        Resource resource = rs.createResource(uri);
+//        resource.getContents().add(EcoreUtil.copy(EcorePackage.eINSTANCE));
+//        String s = resource.toString();
+//        logger.info(s);
+//        String old = new String(
+//                Files.readAllBytes(Paths.get(this.getClass().getClassLoader().getResource("ecore.hron").toURI())
+//                ), StandardCharsets.UTF_8);
+//        Assert.assertEquals(old, s);
+//    }
+//
+//    @Test
+//    public void eStringToStringMapEntryTest() throws IOException {
+//        HronResourceSet rs = new HronResourceSet();
+//        URI uri = URI.createURI("EStringToStringMapEntry.hron");
+//        Resource resource = rs.createResource(uri);
+//        resource.getContents().add(EcoreUtil.copy(EcorePackage.eINSTANCE.getEBigDecimal().getEAnnotations().get(0)));
+//        String s = resource.toString();
+//        logger.info(s);
+//    }
+//
+//    @Test
+//    public void hronTest() throws IOException {
+//        HronResourceSet rs = new HronResourceSet();
+//        URI uri = URI.createURI("HronPackage.hron");
+//        Resource resource = rs.createResource(uri);
+//        resource.getContents().add(EcoreUtil.copy(HronPackage.eINSTANCE));
+//        String s = resource.toString();
+//        logger.info(s);
+//    }
 }
