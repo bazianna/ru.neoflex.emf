4a497d7809bf1a36ed4b0b2b33bc756de18ac5e3
asuvorova <mezzo666@mail.ru>
2022-04-23 00:28:29 +0300

    add solar time

--- timezonedb/src/main/resources/toSolar.sql
+++ timezonedb/src/main/resources/toSolar.sql
@@ -1,11 +1,9 @@
 
 SELECT
-    tz.gmt_offset / 3600,
-       8 - tz.gmt_offset / 3600,
-       DATEADD('minute', DATEDIFF('minute', DATE '1970-01-01', TIMESTAMP '1993-07-05 15:30:00') + ((tz.gmt_offset / 3600 * 15 ) - cb.longitude ) * 4, DATE '1970-01-01') AS solar_time
-FROM zone z JOIN citybase cb ON cb.country_code=z.country_code AND cb.TZ=z.ZONE_NAME
+       DATEADD('minute', DATEDIFF('minute', DATE '1970-01-01', TIMESTAMP '1993-07-05 15:30:00') - (((tz.gmt_offset / 3600 * 15 ) - cb.longitude ) * 4), DATE '1970-01-01') AS solar_time
+FROM zone z JOIN citybase cb ON cb.TZ=z.ZONE_NAME
             JOIN timezone tz ON tz.zone_id=z.zone_id
-WHERE tz.time_start <= DATEDIFF('minute', DATE '1970-01-01', TIMESTAMP '1993-07-05 15:30:00')
-  AND cb.name = 'Ижевск'
+WHERE tz.time_start <= DATEDIFF('second', DATE '1970-01-01', TIMESTAMP '1993-07-05 15:30:00')
+ AND cb.name = 'Ижевск'
 ORDER BY tz.time_start DESC LIMIT 1
 
